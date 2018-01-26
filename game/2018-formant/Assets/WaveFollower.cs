635dae68b07bce6730973ec02b11e44f57d73525
jameswilddev <jameswilddev@outlook.com>
2018-01-26 22:21:31 +0000

    Added a component which tries to follow the wave as understood by the vertex shader.

---
+++ game/2018-formant/Assets/WaveFollower.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveFollower : MonoBehaviour {
+    public Texture2D Wave;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<MeshFilter>().transform.position = new Vector3(0.0f, Wave.GetPixelBilinear((Time.time) + 0.5f, 0).r, 0.0f);
+	}
+}
