c78561de6853b11b131eb55502a5e9a88e2ffd4b
Tim Stoddard <info@gamepopper.co.uk>
2018-01-26 22:55:10 +0000

    Some simple enemy movement behaviours with the player as a target.

---
+++ game/2018-formant/Assets/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public Transform player;
+    public float speed = 5;
+    protected Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+}
