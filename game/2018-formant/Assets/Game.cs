466ea46209f3da7d4ffe6543eacd81b63ae39887
jameswilddev <jameswilddev@outlook.com>
2018-01-27 00:20:50 +0000

    Waves are now instance, and rendered differently.

---
+++ game/2018-formant/Assets/Game.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+    public GameObject WavePrefab;
+
+    public List<Texture2D> WaveTextures = new List<Texture2D>();
+    public readonly List<WaveRenderer> Waves = new List<WaveRenderer>();
+
+	// Use this for initialization
+	void Start () {
+        foreach (var waveTexture in WaveTextures)
+        {
+            var waveRenderer = Instantiate(WavePrefab).GetComponent<WaveRenderer>();
+            waveRenderer.Radius = Waves.Count + 1;
+            waveRenderer.Texture = waveTexture;
+            Waves.Add(waveRenderer);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- game/2018-formant/Assets/WaveFollower.cs
+++ game/2018-formant/Assets/WaveFollower.cs
@@ -16,7 +16,9 @@ public class WaveFollower : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        WavePosition = Time.time;
-        GetComponent<MeshFilter>().transform.position = new Vector3(0.0f, Wave.GetPixelBilinear(WavePosition + 0.5f, 0).r * Amplitude + Radius, 0.0f);
+        ShipPosition += Input.GetAxis("Horizontal") * Time.deltaTime;
+        WavePosition = Time.time * 0.25f;
+        var radius = Wave.GetPixelBilinear(WavePosition - (ShipPosition / Mathf.PI) + 0.5f, 0).r * Amplitude + Radius;
+        GetComponent<MeshFilter>().transform.position = new Vector3(Mathf.Sin(ShipPosition) * radius, Mathf.Cos(ShipPosition) * radius, 0.0f);
 	}
 }
