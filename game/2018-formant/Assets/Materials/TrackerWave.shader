8c6f7235be9b9e7e0fd793ce546ddfa6182e6ce7
jameswilddev <jameswilddev@outlook.com>
2018-01-27 21:57:47 +0000

    Added a tracker-controlled wave.

---
+++ game/2018-formant/Assets/Materials/TrackerWave.shader
@@ -0,0 +1,75 @@
+﻿Shader "Unlit/TrackerWave"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		_Amplitudes ("Amplitudes", Vector) = (0.5, 0.0, 0.0, 0.0)
+		_Positions ("Positions", Vector) = (0.2, 0.0, 0.0, 0.0)
+		_Widths ("Widths", Vector) = (0.7, 0.0, 0.0, 0.0)
+		_Color ("Color", Color) = (1.0, 1.0, 1.0, 1.0)
+		_NoiseAmplitude ("NoiseAmplitude", Float) = 0.25
+		_NoisePosition ("NoisePosition", Float) = 0.0
+		_Radius ("Radius", Float) = 2.0
+		_Thickness ("Thickness", Float) = 0.25
+	}
+	SubShader
+	{
+		Tags{ "Queue" = "Transparent" }
+		LOD 100
+
+		Pass
+		{
+			Blend One One
+			ZWrite Off
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			// make fog work
+			#pragma multi_compile_fog
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				UNITY_FOG_COORDS(1)
+				float4 vertex : SV_POSITION;
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			float4 _Amplitudes;
+			float4 _Positions;
+			float4 _Widths;
+			float4 _Color;
+			float _NoiseAmplitude;
+			float _NoisePosition;
+			float _Radius;
+			float _Thickness;
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+				float2 arc = v.vertex.xy;
+				float noiseSample = tex2Dlod(_MainTex, float4(v.uv.x + _NoisePosition, 0.0, 0.0, 0.0)).r;
+				float scale = _Radius + dot(_Amplitudes, cos(min(abs(v.uv.x - _Positions) / _Widths, 3.142)) * 0.5 + 0.5) + noiseSample * _NoiseAmplitude + (_Thickness * (v.uv.y - 0.5));
+				o.vertex = UnityObjectToClipPos(float3(arc * scale, 0.0));
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				UNITY_TRANSFER_FOG(o, o.vertex);
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				return fixed4(_Color.xyz * pow(abs(sin(i.uv.x * 3.141)), 4.0) * pow(abs(sin(i.uv.y * 3.141)), 16.0), 1.0);
+			}
+			ENDCG
+		}
+	}
+}
---
+++ game/2018-formant/Assets/TrackerWaveRenderer.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackerWaveRenderer : MonoBehaviour, IWaveRenderer
+{
+    public float Radius = 2.0f;
+
+    private Material Material;
+    public bool IsClosest { get; set; }
+
+    private bool WasClosestLastFrame;
+    public float NewlyClosestIntensity;
+    public float ClosestIntensity;
+
+    // Use this for initialization
+    void Start()
+    {
+        Material = GetComponent<MeshRenderer>().material;
+    }
+
+    public Vector4 Amplitudes;
+    public Vector4 Positions;
+    public Vector4 Widths;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsClosest && !WasClosestLastFrame)
+        {
+            NewlyClosestIntensity = 1.0f;
+        }
+        else
+        {
+            NewlyClosestIntensity /= 1.0f + Time.deltaTime * 8.0f;
+        }
+
+        if (IsClosest)
+        {
+            ClosestIntensity = 1.0f;
+        }
+        else
+        {
+            ClosestIntensity /= 1.0f + Time.deltaTime * 8.0f;
+        }
+        WasClosestLastFrame = IsClosest;
+
+        // TODO: This here just redefines the variables used by this class.
+        // These should be filled in when the tracker is ready.
+        Amplitudes = new Vector4(1.0f, 0.0f, 0.0f, 2.0f);
+        Positions = new Vector4(Mathf.Sin(Time.time) * 0.25f + 0.5f, 0.0f, 0.0f, Mathf.Sin(Time.time * 0.25f) * 0.5f + 0.5f);
+        Widths = new Vector4(0.01f, 1.0f, 1.0f, 0.05f);
+
+        Material.SetVector("_Amplitudes", Amplitudes);
+        Material.SetVector("_Positions", Positions);
+        Material.SetVector("_Widths", Widths);
+        Material.SetColor("_Color", Color.Lerp(Color.Lerp(new Color(0.25f, 0.5f, 1.0f), new Color(1.0f, 1.5f, 4.0f), ClosestIntensity), new Color(8.0f, 8.0f, 8.0f), NewlyClosestIntensity));
+        Material.SetFloat("_NoiseAmplitude", 0.1f);
+        Material.SetFloat("_NoisePosition", Time.time * 0.5f);
+        Material.SetFloat("_Radius", Radius);
+        Material.SetFloat("_Thickness", 0.25f);
+    }
+
+    public float RadiusAt(float position)
+    {
+        var output = Radius;
+        output += (Mathf.Cos(Mathf.Min(Mathf.Abs((-position / Mathf.PI) + 0.5f - Positions.x) / Widths.x, Mathf.PI)) * 0.5f + 0.5f) * Amplitudes.x;
+        output += (Mathf.Cos(Mathf.Min(Mathf.Abs((-position / Mathf.PI) + 0.5f - Positions.y) / Widths.y, Mathf.PI)) * 0.5f + 0.5f) *Amplitudes.y;
+        output += (Mathf.Cos(Mathf.Min(Mathf.Abs((-position / Mathf.PI) + 0.5f - Positions.z) / Widths.z, Mathf.PI)) * 0.5f + 0.5f) *Amplitudes.z;
+        output += (Mathf.Cos(Mathf.Min(Mathf.Abs((-position / Mathf.PI) + 0.5f - Positions.w) / Widths.w, Mathf.PI)) * 0.5f + 0.5f) *Amplitudes.w;
+        return output;
+    }
+}
