99317f2989eff0f39d8de16bebcccffa802d7e0e
Kira Hargreaves <kira@hydrolix.co.uk>
2018-01-28 09:00:36 +0000

    updated skybox

---
+++ game/2018-formant/Assets/kira-test/skybox_rotator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skybox_rotator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time); //To set the speed, just multiply the Time
+    }
+}
+ 
